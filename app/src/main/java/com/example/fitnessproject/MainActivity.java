843b97080e591d72447ff5abba0b43b646d75ad2
Engy Ahmed <engya306@gmail.com>
2022-03-29 20:42:33 +0200

    fitness project

---
+++ app/src/main/java/com/example/fitnessproject/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.fitnessproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    PieChart piechart;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        piechart = findViewById(R.id.pie_chart);
+        setupPieChart();
+        loadPieChartData();
+
+    }
+
+    protected void setupPieChart() {
+        piechart.setDrawHoleEnabled(true);
+        piechart.setUsePercentValues(true);
+        piechart.setEntryLabelTextSize(12);
+        piechart.setEntryLabelColor(Color.BLACK);
+        piechart.setCenterText("Calories");
+        piechart.setCenterTextSize(24);
+        piechart.getDescription().setEnabled(false);
+
+        Legend l = piechart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+    }
+
+    private void loadPieChartData() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.2f, "suger"));
+        entries.add(new PieEntry(0.15f, "carb"));
+        entries.add(new PieEntry(0.10f, "fats"));
+        entries.add(new PieEntry(0.25f, "protein"));
+
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color : ColorTemplate.JOYFUL_COLORS) {
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Categories");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(piechart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        piechart.setData(data);
+        piechart.invalidate();
+
+        piechart.animateY(1400, Easing.EaseInOutQuad);
+
+    }
+}
